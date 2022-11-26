3343cec6cd293f93a3ee129546197be79a6f180d
Konstantin Romanov <k-rrr@ya.ru>
2022-11-26 11:39:12 +0300

    some quirk about datetime

--- dateutils.py
+++ dateutils.py
@@ -3,7 +3,6 @@ import datetime
 
 # if used with MSK timezone, it is 06:00 MSK
 def tomorrow_unix_moex():
-    now = datetime.datetime.utcnow()
-    now = now.replace(minute=0, hour=6, second=0,
-                      microsecond=0, tzinfo=datetime.timezone.utc)
+    now = datetime.datetime.now(datetime.timezone.utc)
+    now = now.replace(minute=0, hour=6, second=0, microsecond=0)
     return (now + datetime.timedelta(days=1)).strftime("%s")
