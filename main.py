60506ea01955b3b1617de3ed33d2597c5d4f9c46
Konstantin Romanov <k-rrr@ya.ru>
2022-11-29 14:52:06 +0300

    fix api enum

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
-from fastapi import FastAPI
+from enum import Enum
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from history import HistoryEntry
 from moex import MoexAPI
 from spbex import SpbexAPI
 
+
+class ErrorMessage(BaseModel):
+    status: str
+    description: str
+
+
+class Exchange(str, Enum):
+    Moex = "moex"
+    Spbex = "spbex"
+
+
 EXCHANGE_API_DICT = {
-    "moex": MoexAPI(),
-    "spbex": SpbexAPI(),
+    Exchange.Moex: MoexAPI(),
+    Exchange.Spbex: SpbexAPI(),
 }
 
 
@@ -13,16 +28,31 @@ app = FastAPI()
 
 
 @app.get("/{exchange}/{ticker}")
-async def get_ticker(exchange: str, ticker: str):
+async def get_ticker(exchange: Exchange, ticker: str) -> list[HistoryEntry]:
     exchange = exchange.lower()
     ticker = ticker.lower()
 
     api = EXCHANGE_API_DICT.get(exchange, None)
     if api is None:
-        return {"error": "no api"}
+        return ErrorMessage(**{
+            "status": "error",
+            "description": "not implemented",
+        })
+
     return await api.get_ticker(ticker)
 
 
-@app.get("/healtcheck")
-def healtcheck():
-    return {"status": "ok"}
+@app.get("/healthcheck")
+def healthcheck() -> ErrorMessage:
+    return ErrorMessage(**{
+        "status": "ok",
+        "description": "healthy",
+    })
+
+
+@app.get("/")
+def root_not_implemented() -> ErrorMessage:
+    return ErrorMessage(**{
+        "status": "error",
+        "description": "not implemented",
+    })
